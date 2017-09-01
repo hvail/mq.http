d72073d0cc20842e96c05b5667d45665a8e44056
hvail <415195993@qq.com>
2017-09-01 17:18:46 +0800

    v1.0.3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var version = 'v2.0.0';
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.send("hello world");
+    res.send(version);
 });
 
 module.exports = router;
