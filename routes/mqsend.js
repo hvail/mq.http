9c76ffd228c9af198638d5dadc1fb2060c03c21f
hvail <415195993@qq.com>
2017-12-29 12:04:56 +0800

    v1.4.35-Exchange404

--- routes/mqsend.js
+++ routes/mqsend.js
@@ -50,28 +50,7 @@ let errorShow3 = function (err, a, b, c) {
 };
 
 let _sendMsg = function (ch, ex, tag, msg) {
-    try {
-        ch.checkExchange(ex, function (err, ok) {
-            // console.log(ex + " " + ok);
-            if (err || !ok) {
-                // rabbit.BuildExchange(ex);
-                // ch.assertExchange(ex, 'topic', {durable: false});
-                channel = null;
-                console.log(ex);
-                console.log(err);
-            } else {
-                ch.publish(ex, tag, new Buffer(msg));
-            }
-        });
-        //     .then(function (err, ok) {
-        //     console.log("ch.checkExchange then");
-        //     console.log(err);
-        //     console.log(ok);
-        // });
-        // ch.publish(ex, tag, new Buffer(msg), errorShow, errorShow2, errorShow3);
-    } catch (e) {
-        console.log(e);
-    }
+    ch.publish(ex, tag, new Buffer(msg));
 };
 
 let sendMessage = function (req, res, next) {
