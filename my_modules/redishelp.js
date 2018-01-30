3aa58022061b4fb8cf58858cf7c4e0a54216ecfc
hvail <415195993@qq.com>
2018-01-30 13:29:24 +0800

    v1.5-change-2

--- my_modules/redishelp.js
+++ my_modules/redishelp.js
@@ -1,7 +1,7 @@
 let redis_host = process.env.REDIS_HOST || "119.23.27.9";
 // let redis_pwd = process.env.REDIS_PASSWORD || "892df215f8684736:HvailCom2015";
 let redis_pwd = process.env.REDIS_PASSWORD || "HvailCom2015HYZ";
-redis_pwd = "892df215f8684736:HvailCom2015";
+// redis_pwd = "892df215f8684736:HvailCom2015";
 let redis_port = process.env.REDIS_PORT || 6379;
 const redis = require('redis');
 let redisClient;
